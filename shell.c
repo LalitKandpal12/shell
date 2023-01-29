37a90bfb0f64163297649c61797e62c52ce0f660
Lalit Kandpal <lalitkandpallalit@gmail.com>
2023-01-29 07:31:51 +0000

    adding pipe 1

--- shell.c
+++ shell.c
@@ -15,6 +15,11 @@ void sigHandler(int signo) {
     printf("\n");
 }
 
+void sigHandler2(int signo){
+    printf("signal : %d\n", signo);
+}
+
+
 void split(char* string, char* del, char*arr[]){
     char *token;
     token = strtok(string,del);
@@ -24,6 +29,23 @@ void split(char* string, char* del, char*arr[]){
         token = strtok(NULL, del);
     i++;
     }
+}
+
+
+int pathFinder(char * stringArr[15],char string[30],char * arguments){
+    struct stat buff;
+    for (int i = 0; i < 8; i++){
+        char string2[30];
+        strcpy(string2, stringArr[i]);
+        strcat(string2,"/");
+        strcat(string2, arguments);
+        int ret = stat(string2,&buff);
+        if( ret == 0) {
+            strcpy(string,string2);
+           return 0;
+        }
+    }
+    return -1;
 }
 
 
@@ -40,7 +62,6 @@ int main (void) {
     char * fileName[1];
     int newFd;
     int res, res2, res3, res4;
-    int backGround = 1;
 
     while(1) {
         printf("%s%c%s: ",userName,'%',hostName);
@@ -49,6 +70,8 @@ int main (void) {
         char *argumentArr[10] ={NULL};
         split(arguments," ",argumentArr);
         int i = 0;
+        int execTime = 1;
+        int pipe = 0;
         while(argumentArr[i] != NULL){
             if(strcmp(argumentArr[i],">>")== 0){
                 fileName[0] = argumentArr[i+1];
@@ -63,41 +86,18 @@ int main (void) {
             else if(strcmp(argumentArr[i],"&") == 0){
                 argumentArr[i] = NULL;
                 printf("-----: %d\n",backGround);
-                backGround = 1;
-                // Operator[0] = "&";
+                OperatorUse = -1;
             }
             else if(strcmp(argumentArr[i],"<")==0){
                 fileName[0] = argumentArr[i+1];
                 argumentArr[i] = NULL;
                 OperatorUse = 3;
             }
+            else if(strcmp(argumentArr[i],"|") == 0){
+                pipe++;
+            }
             i++;
         }
-
-        // token = strtok(arguments," ");
-        // int j = 0;
-        // while(token != NULL) {
-        //     if(strcmp(token, ">>")== 0){
-        //         rOperator[0] = ">>";
-        //         // filedes
-        //         printf("break\n");
-        //         token = strtok(NULL," ");
-        //         fileName[1] = token;
-        //         break;
-        //     }
-        //     else if (strcmp(token,">") == 0) {
-        //         rOperator[0] = ">";
-        //         printf("break\n");
-        //         token = strtok(NULL," ");
-        //         fileName[1] = token;
-        //         break;
-        //     }
-        //     argumentArr[j] = token;
-        //     token =  strtok(NULL," ");
-        //     j++;
-        // }
-
-        // argumentArr[9] = NULL;
 
         int pid = fork();
         if(0 != pid){
@@ -105,12 +105,12 @@ int main (void) {
         }
         if(signal(SIGINT,sigHandler)==SIG_ERR);
         if(0 == pid){
+            char string[30];
+            int isPath = pathFinder(stringArr,string,arguments);
             if(arguments[0] == '$'&& arguments[1] == '?'){
                 printf("-bash: %d: command not found\n",childReturnCode);
             }
             else if(OperatorUse != 0) {
-                //if(strcmp(argumentArr[i], ">") == 0 || strcmp(argumentArr[i], ">>") == 0){
-                // printf("inside loop %s this is \n",Operator[0]);
                 if(OperatorUse == 1){
                     newFd = open(fileName[0], O_WRONLY| O_APPEND| O_CREAT,0644);
                     dup2(newFd,1);
@@ -123,30 +123,31 @@ int main (void) {
                     newFd = open(fileName[0],O_RDONLY);
                     dup2(newFd,0);
                 }
-                //}
             }
-            int check = 0;
-            struct stat buff;
-            for (int i = 0; i < 8; i++){
-                char string [20];
-                strcpy(string, stringArr[i]);
-                strcat(string,"/");
-                strcat(string, arguments);
-                int ret = stat(string,&buff);
-                if( ret == 0) {
-                    execv(string,argumentArr);
-                    check = 1;
-                    break;
-                }
+          
+            else if(isPath == 0){
+                execv(string,argumentArr);
             }
-            char *args[] = {arguments,NULL};
-            execv(arguments,args);
-        }else if(backGround == 0) {
+            else {
+                char *args[] = {arguments,NULL};
+                execv(arguments,args);
+            }
+            // for (int i = 0; i < 8; i++){
+            //     char string [20];
+            //     strcpy(string, stringArr[i]);
+            //     strcat(string,"/");
+            //     strcat(string, arguments);
+            //     int ret = stat(string,&buff);
+            //     if( ret == 0) {execv(string,argumentArr);
+            //         check = 1;
+            //         break;
+            //     }
+            // }
+        }else if(OperatorUse != -1) {
             int stat = 0;
             wait(&stat);
             OperatorUse = 0;
             childReturnCode = WEXITSTATUS(stat);
         }
-        backGround = 0;
     }
 }
